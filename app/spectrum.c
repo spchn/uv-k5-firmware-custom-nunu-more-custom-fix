9e09ceaf8e87bab06179dd01e53d73f3f7cbceb4
Niku Toivola <niku.toivola@iki.fi>
2024-05-01 16:57:32 +0300

    bigger bold channel name in spectrum mode

--- app/spectrum.c
+++ app/spectrum.c
@@ -818,6 +818,14 @@ static void DrawF(uint32_t f) {
   sprintf(String, "%u.%05u", f / 100000, f % 100000);
   UI_PrintStringSmall(String, 8, 127, 0);
 
+#if ENABLE_SPECTRUM_SHOW_CHANNEL_NAME
+  if (isKnownChannel) {
+    sprintf(String, "%s", channelName);
+    UI_PrintStringSmallBold(String, 8, 127, 1);
+  } else {
+    UI_PrintStringSmallBold("          ", 8, 127, 1);
+  }
+#endif
   sprintf(String, "%3s", gModulationStr[settings.modulationType]);
   GUI_DisplaySmallest(String, 116, 1, false, true);
   sprintf(String, "%s", bwNames[settings.listenBw]);
